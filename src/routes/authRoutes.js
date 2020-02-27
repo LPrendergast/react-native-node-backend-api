78d1d5f0cc1da84e9c965e832ba3ff46e5cbc16a
Luke Prendergast <luke.prendergast94@gmail.com>
2020-02-27 16:48:43 +0000

    added in bcrpyt for passwords

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,9 +10,29 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
-    res.send({ token});
+    res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password." });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: "Invalid password or email." });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token });
+  } catch {
+    return res.status(401).send({ error: "Invalid password or email." });
   }
 });
 
