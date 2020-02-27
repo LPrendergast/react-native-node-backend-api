b8dc4ef06e49b9a25eb339645b792c4ddd9627db
Luke Prendergast <luke.prendergast94@gmail.com>
2020-02-27 12:54:32 +0000

    added in middleware checking if JWT token is valid and if user has one before showing root

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const { mongoURI } = require("../config");
 const authRoutes = require("./routes/authRoutes");
 const bodyParser = require("body-parser");
+const requireAuth = require("./middlewares/requireAuth");
 const app = express();
 const port = 3000;
 
@@ -23,8 +24,8 @@ mongoose.connection.on("error", err => {
   console.error("Error connecting to Mongo", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi There");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(port, () => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,7 +10,7 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
-    res.send({ token });
+    res.send({ token});
   } catch (err) {
     return res.status(422).send(err.message);
   }
