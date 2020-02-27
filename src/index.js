b59904adc2cfda5f6f2783d959ef5869f3140c04
Luke Prendergast <luke.prendergast94@gmail.com>
2020-02-27 11:25:20 +0000

    Added in Mongodb server connection utils, setup node server locally

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { mongoURI } = require("../config");
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+const port = 3000;
+
+app.use(authRoutes);
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo instance");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to Mongo", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
